6a38c987fec5e64d3410351b52a94f73a245298a
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-04 19:09:18 +0200

    Set up basic registration route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,19 @@ const router = express.Router();
 
 // Add a new user
 router.post("/register", async (req, res) => {
-    console.log("Register called...")
+    try {
+        const { username, password } = req.body;
+
+        // Validate input (VIDAREUTVECKLA INNAN INLÄMNING, både validering och 400 meddelande)
+        if (!username || !password ) {
+            return res.status(400).json({ error: "Invalid input. Insert username and password."});
+        }
+
+        // If ok - save user 
+        res.status(201).json({ message: "User created successfully."});
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
 // User login
