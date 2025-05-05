b377877fb756852850fde6cba12f5ceb61cc5b19
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-05 10:44:50 +0200

    Store input and hash password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,12 @@
 /* Routes for authentication */
-
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
+const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
+
+// Connect to database 
+const db = new sqlite3.Database(process.env.DATABASE);
 
 // Add a new user
 router.post("/register", async (req, res) => {
@@ -9,12 +14,24 @@ router.post("/register", async (req, res) => {
         const { username, password } = req.body;
 
         // Validate input (VIDAREUTVECKLA INNAN INLÄMNING, både validering och 400 meddelande)
-        if (!username || !password ) {
-            return res.status(400).json({ error: "Invalid input. Insert username and password."});
+        if (!username || !password) {
+            return res.status(400).json({ error: "Invalid input. Insert username and password." });
         }
 
-        // If ok - save user 
-        res.status(201).json({ message: "User created successfully."});
+        // Hash password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        // Check if user exists - på egen hand
+
+        // Save user if input is correct
+        const sql = `INSERT INTO users(username, password) VALUES(?,?)`;
+        db.run(sql, [username, hashedPassword], (err) => {
+            if (err) {
+                res.status(400).json({ message: "Error creating user..." })
+            } else {
+                res.status(201).json({ message: "User created successfully." });
+            }
+        });
     } catch (error) {
         res.status(500).json({ error: "Server error" });
     }
@@ -26,15 +43,15 @@ router.post("/login", async (req, res) => {
         const { username, password } = req.body;
 
         // Validate input (VIDAREUTVECKLA INNAN INLÄMNING, både validering och 400 meddelande)
-        if (!username || !password ) {
-            return res.status(400).json({ error: "Invalid input. Insert username and password."});
+        if (!username || !password) {
+            return res.status(400).json({ error: "Invalid input. Insert username and password." });
         }
 
         // Check credentials 
-        if(username === "julia" && password === "password") {
-            res.status(200).json({ message: "Login successful"});
+        if (username === "julia" && password === "password") {
+            res.status(200).json({ message: "Login successful" });
         } else {
-            res.status(401).json({ error: "Invalid username/password"});
+            res.status(401).json({ error: "Invalid username/password" });
         }
     } catch (error) {
         res.status(500).json({ error: "Server error" });
