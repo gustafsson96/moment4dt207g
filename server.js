4b787994f658104637d3bc3b48305916a0a2015b
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-05 19:55:17 +0200

    Set protected route to display fun facts

--- server.js
+++ server.js
@@ -4,16 +4,27 @@ const authRoutes = require("./routes/authRoutes");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(process.env.DATABASE)
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Routes
+
 app.use("/api", authRoutes);
 
-// Protected routes
+// Protected route that returns fun facts
 app.get("/api/protected", authenticateToken, (req, res) => {
-    res.json({ message: "Protected route accessed"});
+    const factsQuery = `SELECT * FROM funfacts`;
+
+    db.all(factsQuery, [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ message: "Failed to fetch fun facts." })
+        }
+        res.json({ funFacts: rows });
+    });
 });
 
 // Validate token
@@ -21,10 +32,10 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1]; // Token
 
-    if (token == null) res.status(401).json({ message: "Not authorized for this route. Token missing."});
+    if (token == null) res.status(401).json({ message: "Not authorized for this route. Token missing." });
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
-        if(err) return res.status(403).json({ message: "Invalid JWT."});
+        if (err) return res.status(403).json({ message: "Invalid JWT." });
 
         req.username = username;
         next();
